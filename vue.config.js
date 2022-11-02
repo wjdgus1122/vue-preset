0699418ac614ca80402ee58cde4a48593e1884b6
wjdgus1122 <qodqodantk@gmail.com>
2022-11-02 12:03:34 +0900

    vue study - component edit

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  chainWebpack : config => {
+    config.plugin.delete('prefetch');
+  }
+}
